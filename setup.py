88453341cc99a9a8808273f5957bc96f18c6a01c
Robin 'r0w' Weiland <robinweiland@gmx.de>
2020-08-30 18:06:49 +0200

    created setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,71 @@ __version__ = "0.0.0"
 
 __all__ = ()
 
+from pathlib import Path
+from setuptools import setup, find_packages
+from shutil import rmtree
 
-if __name__ == '__main__': pass
+BUILD_DIRS = (
+    'jsonvc.egg-info',
+    'build',
+    'dist'
+)
+
+
+if __name__ == '__main__':
+    print('cleaning up')
+    for d in BUILD_DIRS:
+        path = Path(d)
+        if path.exists() and path.is_dir():
+            print(f'Deleting {path}...')
+            rmtree(d)
+
+    setup(
+        name='jsonvc',
+        version=__version__,
+        packages=find_packages(),
+        url='https://github.com/robin-weiland/jsonvc',
+        license='MIT',
+        author=__author__,
+        author_email='robin.weiland@gmx.de',
+        description='tiny version control for json data',
+        long_description=Path('README.md').read_text(),
+        long_description_content_type='text/markdown',
+        keywords=['json', 'vcs', 'git'],
+        python_requires='>=3.6',  # due to __class_getitem__  see PEP560
+        classifiers=[
+            'Development Status :: 4 - Beta',
+
+            'Environment :: Console',
+
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+
+            'License :: OSI Approved :: MIT License',
+
+            'Operating System :: OS Independent',
+
+            'Programming Language :: Python :: 3 :: Only',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+
+
+            'Topic :: Software Development',
+            'Topic :: Software Development :: Version Control',
+            'Topic:: Scientific / Engineering',
+            'Topic :: Scientific/Engineering :: Information Analysis',
+
+            'Typing::Typed'
+        ],
+        requires=[line.split('=')[0].rstrip('~<>=')
+                  for line in Path('requirements.txt').read_text().splitlines()
+                  if not (line.startswith('#') or line == '' or line == '\n' or line.endswith('###'))],
+        entry_points={
+            'console_scripts': [
+                'jsonvc = jsonvc:main'
+            ]
+        }
+    )
